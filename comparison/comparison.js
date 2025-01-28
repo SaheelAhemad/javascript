09903e8cfeea77ac9b125cccefe8cebaddb211de
Saheel <saheelahemad20112003@gmail.com>
2025-01-28 07:37:59 +0530

    Comparison and operations of javascript

---
+++ comparison/comparison.js
@@ -0,0 +1,12 @@
+console.log(2>"1");
+console.log(1>"2");
+console.log("2">"1");
+console.log(2>1);
+
+console.log(null>0);
+console.log(null<0);
+console.log(null>=0);
+
+// Strict check
+console.log(2===2);
+console.log("2"===2);
---
+++ operations/operations.js
@@ -0,0 +1,20 @@
+let value=3;
+let negVal=-value;
+console.log(negVal);
+
+let str1="hello";
+let str2=" world";
+let str3=str1+str2;
+console.log(str3);
+
+console.log(1+2);
+console.log("1"+2);
+console.log("1"+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+console.log(true);
+
+let num1, num2, num3;
+num1=num2=num3=10;
+console.table([num1, num2, num3]);
